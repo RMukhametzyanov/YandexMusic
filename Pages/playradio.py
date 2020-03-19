f4efed3cf9e12796fed7d0d38c7d14a5468c6226
RMukhametzyanov <oki01@mail.ru>
2020-03-19 22:32:09 +0500

    Добавлены функции удаления песни и проверка после удаления

--- Pages/playradio.py
+++ Pages/playradio.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from Pages.BaseApp import BasePage
 from Functions.Highlight import highlight
+from selenium.webdriver.common.action_chains import ActionChains #Для ховера на песне в плей-листе
 import time
 import allure
 
@@ -22,6 +23,8 @@ class PlayRadioPageLocators:
     LOCATOR_LikeSongNameInMyPlaylist =      (By.CSS_SELECTOR, ".d-track__name")                 # Название первой песни в моем плейлисте
     LOCATOR_LikeSongArtistInMyPlaylist =    (By.CSS_SELECTOR, ".d-track__artists")              # Название исполнителя первой песни в моем плейлисте
 
+    LOCATOR_DeleteSong_button =             (By.CSS_SELECTOR, "[title='Удалить трек']")         # Удаление песни их плей-листа
+    LOCATOR_PlaylistEmpty =                 (By.CSS_SELECTOR, ".playlist-dummy__title")         # Все песни в плейлисте
 class PlayRadio(BasePage):
 
     """
@@ -129,3 +132,41 @@ class PlayRadio(BasePage):
             assert (element_success_songartist == like_song_artist), \
                 "Эталонный текст не равен текущему.\n" + \
                 "Текущий: " + element_success_songartist + "\nЭталонный: " + like_song_artist
+
+    """
+            Функция удаления песни. Делает ховер по найденной песне и удаляет.
+     """
+
+    @allure.step("Удаление песни из плейлиста")
+    def delete_song(self):
+        with allure.step("Поиск песни в плейлисте"):
+            name_song_for_delete = self.find_element(PlayRadioPageLocators.LOCATOR_LikeSongNameInMyPlaylist)
+            highlight(name_song_for_delete)
+
+        with allure.step("Ховер по песне для отображения кнопки 'удалить'"):
+            hover = ActionChains(self.driver).move_to_element(name_song_for_delete)
+            hover.perform()
+
+        with allure.step("Поиск и клик по кнопке 'Удаление'"):
+            delete_song = self.find_element(PlayRadioPageLocators.LOCATOR_DeleteSong_button)
+            highlight(delete_song)
+            delete_song.click()
+
+    """
+        Функция проверки на пустой плейлист. 
+    """
+
+    #
+    # ПЕРЕДЕЛАТЬ, ЭТО НИКУДА НЕ ГОДИТСЯ!!! А ЕСЛИ В ПЛЕЙЛИСТЕ НЕСКОЛЬКО ПЕСЕН?
+    #
+
+    @allure.step("Проверка на пустой плейлист")
+    def check_song_deleted(self):
+        with allure.step("Проверяем наличие 'Плейлист пока пуст'"):
+            # Эталонный текст
+            success_text = "Плейлист пока пуст"
+            element_success = self.find_element(PlayRadioPageLocators.LOCATOR_PlaylistEmpty).text
+            # Сравниваем полученный текст от элемента с эталонным
+            assert (element_success == success_text), \
+                "Эталонный текст не равен текущему.\n" + \
+                "Текущий: " + element_success + "\nЭталонный: " + success_text
--- Tests/test_cases.py
+++ Tests/test_cases.py
@@ -1,6 +1,7 @@
 from Pages.authorization import Authorization
 from Pages.playradio import PlayRadio
-from Pages.clean_playlist import CleanPlaylist
+from Pages.create_playlist import CreateNewPlaylist
+import time
 
 #smoke
 def test_authorization(browser):
@@ -25,17 +26,24 @@ def test_playradio(browser):
     playradio_main_page.my_music()
     playradio_main_page.like_song_playlist()
     playradio_main_page.check_like_music()
+    playradio_main_page.delete_song()
+    playradio_main_page.check_song_deleted()
 
-#acceptance
-def test_clean_play_list(browser):
+
+def test_create_playlist(browser):
     authorization_main_page = Authorization(browser)
     authorization_main_page.go_to_site()
     authorization_main_page.login_button()
     authorization_main_page.sign_in('ForTestLogin', 'ForTestLogin123')
     authorization_main_page.sign_in_check()
+
     playradio_main_page = PlayRadio(browser)
     playradio_main_page.my_music()
-    playradio_main_page.like_song_playlist()
-    cleanplaylist_main_page = CleanPlaylist(browser)
-    cleanplaylist_main_page.delete_song()
-    cleanplaylist_main_page.check_song_deleted()
+
+    create_playlist_main_page = CreateNewPlaylist(browser)
+    create_playlist_main_page.create_playlist()
+    create_playlist_main_page.rename_new_playlist()
+
+    playradio_main_page.my_music()
+
+    create_playlist_main_page.delete_new_playlist()
